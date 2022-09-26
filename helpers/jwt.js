379961b32a7fd41d2229fb2d4828a76390258db6
Boris Zarate <boris.zlobos@gmail.com>
2022-09-25 21:05:20 -0300

    add jwt

---
+++ helpers/jwt.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+
+
+const generateJWT = (uid, name) => {
+
+  return new Promise((resolve, reject) => {
+
+    const payload = { uid, name }
+
+    jwt.sign(payload, process.env.SECRED_JWT_SEED, {
+      expiresIn: '2h'
+    }, (err, token) => {
+      if (err) {
+        console.log( err )
+        reject('No se pude generar el token')
+      }
+      resolve( token )
+    })
+  })
+
+}
+
+
+module.exports = {
+  generateJWT
+}
