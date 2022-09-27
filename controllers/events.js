755ab6ba45da5ece13d3f95d621a88f4a39982bd
Boris Zarate <boris.zlobos@gmail.com>
2022-09-27 19:59:47 -0300

    feat: update event

--- controllers/events.js
+++ controllers/events.js
@@ -1,10 +1,15 @@
 const { response } = require('express');
 const Event = require('../models/Event')
 
-const getEvents = (req, res = response) => {
+const getEvents = async(req, res = response) => {
+
+	const events = await Event
+		.find()
+		.populate('user', 'name')
+
 	res.json({
 		ok: true,
-		msg: 'getEvents',
+		events
 	});
 };
 
@@ -30,12 +35,52 @@ const createEvent = async(req, res = response) => {
 		})
 	}
 };
-const updateEvent = (req, res = response) => {
-	res.json({
-		ok: true,
-		msg: 'updateEvent',
-	});
+
+const updateEvent = async(req, res = response) => {
+	
+	const eventId = req.params.id
+	const uid = req.uid
+
+	try {
+
+		const event = await Event.findById( eventId )
+
+		if (!event) {
+			res.status(404).json({
+				ok: false,
+				msg: 'Evento no existe por ese id'
+			})
+		}
+
+		if ( event.user.toString() !== uid ) {
+			return res.status(401).json({
+				ok: false,
+				msg: 'No tiene privilegio de editar este evento'
+			})
+		}
+
+		const newEvent = {
+			...req.body,
+			user: uid
+		}
+
+		const eventUpdated = await Event.findByIdAndUpdate(eventId, newEvent, { new: true })
+
+		res.json({
+			ok: true,
+			event: eventUpdated
+		})
+
+	} catch(error) {
+			console.log(error)
+		res.status(500).json({
+			ok: false,
+			msg: 'Hable con el administrador'
+		})
+	}
+
 };
+
 const deleteEvent = (req, res = response) => {
 	res.json({
 		ok: true,
