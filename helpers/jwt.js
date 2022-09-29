43842381ae8f010b38ed93331ff9871c07c53156
Boris Zarate <boris.zlobos@gmail.com>
2022-09-28 22:08:09 -0300

    fix jwt

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -7,7 +7,7 @@ const generateJWT = (uid, name) => {
 
     const payload = { uid, name }
 
-    jwt.sign(payload, process.env.SECRED_JWT_SEED, {
+    jwt.sign(payload, process.env.SECRET_JWT_SEED, {
       expiresIn: '2h'
     }, (err, token) => {
       if (err) {
